6195f8c2e9231ec348c7978dca26fca5ca4b705f
Kerem <keremkartal491741@gmail.com>
2025-12-28 22:17:53 +0300

    Master, Service, Location modelleri ve MasterProfileRepository eklendi

---
+++ Backend/MasterFind/Repositories/Master/IMasterProfileRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Repositories.Master
+{
+    public interface IMasterProfileRepository : IGenericRepository<MasterProfile>
+    {
+        /// <summary>
+        /// Belirli bir hizmet kategorisi ve/veya konuma göre ustaları arar.
+        /// İlişkili verileri (Hizmetler, Konumlar) de yükler.
+        /// </summary>
+        Task<IEnumerable<MasterProfile>> SearchMastersAsync(int? serviceCategoryId, int? locationId);
+
+        /// <summary>
+        /// Bir kullanıcının AppUserId'sine göre Usta profilini getirir.
+        /// </summary>
+        Task<MasterProfile?> GetByAppUserIdAsync(string appUserId); 
+        Task<IEnumerable<ServiceCategory>> GetServiceCategoryWithServicesAsync();
+        Task<MasterProfile?> GetByIdWithAllDetailsAsync(int masterProfileId);
+
+    }
+}
